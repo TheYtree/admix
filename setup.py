764b1bf08b54b92d8be7bd49cd5d5956312613d3
lyz <linyize@gmail.com>
2025-07-12 08:38:53 +0800

    更新setup.py，包含position_models目录到打包发布中

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='admix',
 
-    version='1.2',
+    version='1.3',
 
     description='An admixture analysis tool that supports raw data from 23andme, AncestryDNA, etc.',
     long_description=open('admix/README.rst','r').read(),
 
-    url='https://github.com/stevenliuyi/admix',
+    url='https://github.com/TheYtree/admix',
 
     author='Steven Liu',
     author_email='me@yliu.io',
@@ -26,17 +26,21 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
     ],
 
-    keywords='bio DNA SNP ancestry admixture',
+    keywords='bio DNA SNP ancestry admixture position-based',
 
     install_requires=['numpy','scipy'],
 
     packages=['admix'],
 
     package_data={
-        'admix':['data/*', 'README.rst']
+        'admix':['data/*', 'data/position_models/*', 'README.rst']
     },
 
     entry_points={
